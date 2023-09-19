a4085e8283f77102b20ef75255935cf65ab7fa64
Ezra Andersen <stardustezra@gmail.com>
2023-09-19 19:59:55 +0200

    variabel forståels

--- wpc01/javascript_leg/promises.js
+++ wpc01/javascript_leg/promises.js
@@ -3,18 +3,17 @@ const fs = require('fs');
 function getDirFiles(){
     return new Promise (function(resolve, reject) {
         fs.readdir(__dirname, function(err, files) {
-        if(err) return reject(err);
-        resolve(files);    
+            if(err) return reject(err);
+            resolve(files);    
         });
     });
 }
 
 getDirFiles()
     .then(function(files) {
-        console.log('THEN',files);
+        console.log('THEN', files);
 
     })
-
     .catch(function(err) {
         console.error('CATCH', err);
 
---
+++ wpc01/javascript_leg/variable-test.js
@@ -0,0 +1,38 @@
+function getUserName(users, id) { 
+    let user = users.find( //leder efter bruger(user)
+        function (user) {
+            return user.id === id
+        }
+    )
+    if (!user) return null //fandt ikke user
+    return user.name //fandt user
+}
+
+function sayHello(users, id) {
+    let output = "Hej!"
+    const userName = getUserName(users, id) //telefonkø til getUserName
+
+    if (userName !== null) { //tjekker om der er værdig fra getUserName
+        output = "Hej, " + userName + "!"
+    }
+    return output
+}
+
+
+const users = [ //array
+    {
+        id: 1,
+        name: "Albert"
+    },
+    {
+        id: 2,
+        name: "Anne-lise"
+    },
+    {
+        id: 3,
+        name: "Holger-preben"
+    },
+]
+
+console.log(sayHello(users, 1)) //kalder funktion
+console.log(sayHello(users, 5))
