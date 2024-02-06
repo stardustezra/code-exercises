3742c826205afffc123cebc5bb03f3b61b4a5ae3
Ezra Andersen <stardustezra@gmail.com>
2024-02-06 13:17:58 +0100

    opgave 2

--- wpc01/templates/template-fun/src/components/Textarea.vue
+++ wpc01/templates/template-fun/src/components/Textarea.vue
@@ -1,20 +1,25 @@
 <script setup>
 import { ref } from "vue";
 
-//Model
+// Model
 const textAreaContent = ref("");
-const displayedContent = ref("");
+const previousInputs = ref([]);
 
-//ViewModel
+// ViewModel
 const clickButton = () => {
-  displayedContent.value = textAreaContent.value;
+  previousInputs.value.push(textAreaContent.value);
+  textAreaContent.value = ""; // Clear the textarea after sending
 };
 </script>
 
 <template>
-  <textarea v-model="textAreaContent">Enter text</textarea>
-  <button @click="clickButton">Send</button>
-  <div>{{ displayedContent }}</div>
+  <div>
+    <textarea v-model="textAreaContent">Enter text</textarea>
+    <button @click="clickButton">Send</button>
+  </div>
+  <ul>
+    <li v-for="input in previousInputs" :key="input">{{ input }}</li>
+  </ul>
 </template>
 
 <style lang="scss" scoped></style>
